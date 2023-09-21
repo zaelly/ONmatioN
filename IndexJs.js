a09800f6ddd6e62b92331346a6d8aec55fd831ac
zaelly <zaellybarbosa@gmail.com>
2023-09-21 00:24:33 -0300

    NAVBAR in process

---
+++ IndexJs.js
@@ -0,0 +1,6 @@
+document.getElementById("hamburguer-icon").onclick = function(){
+    document.getElementById("sliding-header-menu-outer").style.right = "0px";
+  };
+  document.getElementById("sliding-header-menu-close-button").onclick = function(){
+    document.getElementById("sliding-header-menu-outer").style.right = "-320px";
+  };
