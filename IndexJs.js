6d694f4a9776409ac26f47958e2cf66a84961c1e
zaelly <zaellybarbosa@gmail.com>
2023-09-22 17:16:31 -0300

    style navbar completo

--- IndexJs.js
+++ IndexJs.js
@@ -4,3 +4,8 @@ document.getElementById("hamburguer-icon").onclick = function(){
   document.getElementById("sliding-header-menu-close-button").onclick = function(){
     document.getElementById("sliding-header-menu-outer").style.right = "-320px";
   };
+
+ let dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+let dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+  return new bootstrap.Dropdown(dropdownToggleEl)
+})
