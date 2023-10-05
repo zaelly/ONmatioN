60f6db16c06e08561086de2a8aabec632b5db379
zaelly <zaellybarbosa@gmail.com>
2023-10-05 02:28:30 -0300

    arrumando tela mobile

--- IndexJs.js
+++ IndexJs.js
@@ -1,16 +1,22 @@
-document.getElementById("hamburguer-icon").onclick = function(){
-    document.getElementById("sliding-header-menu-outer").style.right = "0px";
-  };
-  document.getElementById("sliding-header-menu-close-button").onclick = function(){
-    document.getElementById("sliding-header-menu-outer").style.right = "-320px";
-  };
+// document.getElementById("hamburguer-icon").onclick = function(){
+//     document.getElementById("sliding-header-menu-outer").style.right = "0px";
+//   };
+//   document.getElementById("sliding-header-menu-close-button").onclick = function(){
+//     document.getElementById("sliding-header-menu-outer").style.right = "-320px";
+//   };
 
- let dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
-let dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-  return new bootstrap.Dropdown(dropdownToggleEl)
-})
+//  let dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+// let dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+//   return new bootstrap.Dropdown(dropdownToggleEl)
+// })
 
-document.getElementById("button-mode").addEventListener("click", function(){
-  document.querySelector("body").setAttribute("class","dark");
+document.querySelector('.ball').addEventListener('click', (e)=>{
+  e.target.classList.toggle('ball-move');
+  document.body.classList.toggle('dark');
 });
+let alphabet = document.getElementById('alphabet');
 
+document.querySelector('.dropdown-menu').addEventListener('click', function () {
+
+  
+})
