d47d5850b49c885987f516cd5afe884d0c1dd3e5
zaelly <zaellybarbosa@gmail.com>
2023-12-02 10:21:33 -0300

    att

--- Js/IndexJs.js
+++ Js/IndexJs.js
@@ -23,6 +23,39 @@ document.querySelector('.ball').addEventListener('click', (e)=>{
           element.style.color = 'black';
       }
   });
+
+//   const changeUnder = document.querySelector('Tracos');
+
+//   changeUnder.forEach(e => {
+//     if(e.style.backgroundColor === "black" || e.style.backgroundColor === ""){
+//         e.style.backgroundColor = "white";
+//     } else{
+//         e.style.backgroundColor = "black";
+//     }
+//   });
 })
 
+const arrastavel = document.getElementById('arrastavel');
+let isMouseDown = false;
+let startX, scrollLeft;
+
+arrastavel.addEventListener('mousedown', (e) => {
+  isMouseDown = true;
+  startX = e.pageX - arrastavel.offsetLeft;
+  scrollLeft = arrastavel.scrollLeft;
+});
+
+arrastavel.addEventListener('mouseup', () => {
+  isMouseDown = false;
+});
+
+arrastavel.addEventListener('mousemove', (e) => {
+  if (!isMouseDown) return;
+  e.preventDefault();
+  const x = e.pageX - arrastavel.offsetLeft;
+  const walk = (x - startX) * 1;
+  arrastavel.scrollLeft = scrollLeft - walk;
+});
+
+  
 
